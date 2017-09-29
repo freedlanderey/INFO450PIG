0e6be471a70f3d19adaf804ca29e4562f699fd48
Ezra Freedlander <freedlanderey@vcu.edu>
2017-09-28 22:33:08 -0400

    Trying stuff out but failing...

--- PigLatinTranslator/PigLatinTranslator.cpp
+++ PigLatinTranslator/PigLatinTranslator.cpp
@@ -1,13 +1,33 @@
 // PigLatinTranslator.cpp : Defines the entry point for the console application.
 //
 
-#include <iostream>
 #include "stdafx.h"
+#include <iostream>
+#include <stdio.h>
 
 using namespace std;
 
 int main()
 {
+	char wordArray[50], pigLatin[50];
+	char firstLetter[2];
+	
+	gets_s(wordArray);
+
+	cout << wordArray << endl;
+
+	cout << wordArray[0] << endl;
+
+	for (int i = 0; i < 50; i++)
+	{
+		pigLatin[i] = wordArray[i] + wordArray[0];
+	}
+
+	cout << pigLatin << endl;
+
+	//strcat_s(pigLatin, wordArray);
+	//strcat_s(pigLatin, firstLetter);
+
     return 0;
 }
 
