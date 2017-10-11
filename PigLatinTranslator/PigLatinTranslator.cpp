9b317ca642f6f253d59987047da57cc2940352fd
Ezra Freedlander <freedlanderey@vcu.edu>
2017-10-10 23:14:53 -0400

    Added some final touches

--- PigLatinTranslator/PigLatinTranslator.cpp
+++ PigLatinTranslator/PigLatinTranslator.cpp
@@ -11,35 +11,50 @@ int main()
 {
 	char wordArray[50];
 	char answer;
-	int pigLatinTranslator(char Array[]);
+	int translateToPigLatin(char Array[]);
 	void vowelCheck(char vowel[]);
+	int i = 0;
 
 	do
 	{
-		cout << "Enter a word to be translated in to pig latin." << endl;		
+		cout << "Enter a word to be translated into pig latin." << endl;
 		cin >> wordArray;
 
-		if (wordArray[0] == 'A' || wordArray[0] == 'a' || wordArray[0] == 'E' || wordArray[0] == 'e' ||
-			wordArray[0] == 'I' || wordArray[0] == 'i' || wordArray[0] == 'O' || wordArray[0] == 'o' ||
-			wordArray[0] == 'U' || wordArray[0] == 'u')
-		{
-			cout << wordArray << "ay" << endl;
-		}
-		else
-		{
-			pigLatinTranslator(wordArray);
-		}
+				if (strcmp(wordArray, "the") == 0 || strcmp(wordArray, "and") == 0 || strcmp(wordArray, "but") == 0 ||
+					strcmp(wordArray, "for") == 0 || strcmp(wordArray, "nor") == 0 || strcmp(wordArray, "yet") == 0)
+				{
+					cout << wordArray << endl;
+				}
+				else
+				{
+					if (wordArray[0] == '\0' || wordArray[1] == '\0' || wordArray[2] == '\0')
+					{
+						cout << wordArray << endl;
+					}
+					else
+					{
+						if (wordArray[0] == 'A' || wordArray[0] == 'a' || wordArray[0] == 'E' || wordArray[0] == 'e' ||
+							wordArray[0] == 'I' || wordArray[0] == 'i' || wordArray[0] == 'O' || wordArray[0] == 'o' ||
+							wordArray[0] == 'U' || wordArray[0] == 'u')
+						{
+							cout << wordArray << "ay" << endl;
+						}
+						else
+						{
+							translateToPigLatin(wordArray);
+						}
+					}
+				}
 
-		cout << "Do another? (Enter Y for yes.)" << endl;
-		cin >> answer;
+			cout << "Do another? (Enter Y for yes.)" << endl;
+			cin >> answer;
 
 	} while ((answer == 'Y') || (answer == 'y'));
-
 
     return 0;
 }
 
-int pigLatinTranslator(char wordArray[])
+int translateToPigLatin(char wordArray[])
 {
 	char pigLatin[50];
 
